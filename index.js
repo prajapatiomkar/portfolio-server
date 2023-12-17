65a0b3fc2180560886e9e7c51fe4535ec84fb9b0
Omkar Prajapati <72141037+prajapatiomkar@users.noreply.github.com>
2023-12-17 23:04:27 +0530

    portfolio server init.

---
+++ index.js
@@ -0,0 +1,27 @@
+import { configDotenv } from "dotenv";
+configDotenv();
+import express from "express";
+import cors from "cors";
+const app = express();
+
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/api", (req, res) => {
+  res.send("hello from server!");
+});
+
+// Global error handler - route handlers/middlewares which throw end up here
+app.use((err, req, res, next) => {
+  // response to user with 403 error and details
+});
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});

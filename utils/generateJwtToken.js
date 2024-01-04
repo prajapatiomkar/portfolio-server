08a52077072641a5d229b8bd82f98c2e75ecc71c
Omkar Prajapati <72141037+prajapatiomkar@users.noreply.github.com>
2024-01-04 10:47:04 +0530

    made changes in logic server.

--- utils/generateJwtToken.js
+++ utils/generateJwtToken.js
@@ -7,7 +7,6 @@ const generateJwtToken = (res, _id) => {
 
   res.cookie("jwt", token, {
     httpOnly: true,
-    // secure: process.env.NODE_ENV !== "development",
     secure: true,
     sameSite: "None",
     maxAge: 1 * 24 * 60 * 60 * 1000,
